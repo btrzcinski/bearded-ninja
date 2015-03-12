cfe37011fa2d8a13a79d09d92f90820b4f064456
Barnett Trzcinski <btrzcinski@gmail.com>
2015-03-12 12:30:51 -0700

    Build script for other generators

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CMAKEOPTS=
+
+if [ "$1" != "" ]; then
+	CMAKEOPTS="-G $1"
+fi
 
 mkdir -p $DIR/build
 cd $DIR/build
-cmake ..
+cmake $CMAKEOPTS ..
