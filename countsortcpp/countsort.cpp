1dd94dcaac90009e22fbc1b52f521242716434e2
Barnett Trzcinski <btrzcinski@gmail.com>
2016-03-24 23:50:01 -0700

    Touchups and convert strings to std::string

--- countsortcpp/countsort.cpp
+++ countsortcpp/countsort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <random>
@@ -6,20 +7,21 @@
 struct student
 {
 	unsigned id;
-	std::wstring name;
+	std::string name;
 
-	operator std::wstring() 
+	operator std::string() const
 	{
-		auto strRep = std::wstring{L"id = "} + std::to_wstring(id) + L", name=" + name;
-		return strRep;
+        std::ostringstream strRep;
+        strRep << "id = " << id << ", name=" << name;
+        return strRep.str();
 	}
 };
 
 void printclass(std::vector<student>& c)
 {
-	for(auto&& s : c)
+	for(auto const& s : c)
 	{
-		std::wcout << static_cast<std::wstring>(s) << std::endl;
+		std::cout << static_cast<std::string>(s) << std::endl;
 	}
 }
 
@@ -42,9 +44,9 @@ unsigned countsort(std::vector<student>& c)
 void shuffle(std::vector<student>& c)
 {
 	static auto rando = std::minstd_rand{std::random_device{}()};
-	for(auto i = 0U; i < 50; ++i)
+	for(auto i = 0U; i < c.size(); ++i)
 	{
-		std::swap(c[i],c[rando() % 50]);
+		std::swap(c[i],c[rando() % c.size()]);
 	}
 }
 
@@ -53,17 +55,17 @@ int main(int argc, char *argv[])
 	auto c = std::vector<student>{};
 	for(auto i = 0U; i < 50; ++i)
 	{
-		auto name = std::wstring{L"Student"} + std::to_wstring(i);
+		auto name = std::string{"Student"} + std::to_string(i);
 		c.push_back(student{i, name});
 	}
 
 	printclass(c);
-	std::wcout << std::endl;
+	std::cout << std::endl;
 	shuffle(c);
 	printclass(c);
-	std::wcout << std::endl;
+	std::cout << std::endl;
 
-	std::wcout << L"swaps = " << countsort(c) << std::endl;
+	std::cout << "swaps = " << countsort(c) << std::endl;
 	printclass(c);
 
 	return 0;
