350eaca2b25070f8fabb6103c3b29f7432191c24
Barnett Trzcinski <btrzcinski@gmail.com>
2016-08-22 16:06:13 -0700

    Scripts operate on piping streams rather than FIFO

--- webradio/play.sh
+++ webradio/play.sh
@@ -2,29 +2,12 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [[ "$1" = "" ]]; then
-    echo Using fifo = 977fifo
-    FIFO_PATH=$DIR/977fifo
-else
-    FIFO_PATH=$1
-fi
-
-if [[ ! -e $FIFO_PATH ]]; then
-    mkfifo $FIFO_PATH
-fi
-
-if [[ ! -p $FIFO_PATH ]]; then
-    echo $FIFO_PATH is not a valid FIFO
-    exit
-fi
-
 if [[ ! -d $DIR/logs ]]; then
     mkdir $DIR/logs
 fi
 
-$DIR/webradio.out $FIFO_PATH 2>$DIR/logs/webradio.log &
-echo $! > $DIR/webradio.pid
-gst-launch-1.0 filesrc location=$FIFO_PATH ! aacparse ! faad ! audioconvert ! alsasink >logs/gstreamer.log 2>&1 &
+( $DIR/webradio.out 2>$DIR/logs/webradio.log & echo $! >$DIR/webradio.pid ) |
+    gst-launch-1.0 fdsrc ! aacparse ! faad ! audioconvert ! alsasink >logs/gstreamer.log 2>&1 &
 echo $! > $DIR/gstreamer.pid
 
 echo Started
--- webradio/stop.sh
+++ webradio/stop.sh
@@ -8,16 +8,16 @@ if [[ ! -e $DIR/webradio.pid ]]; then
 fi
 
 WEBRADIOPID=`cat $DIR/webradio.pid`
-rm $DIR/webradio.pid
 
 echo Stopping WebRadio [ PID = $WEBRADIOPID ]
 kill $WEBRADIOPID
+rm $DIR/webradio.pid
 
 GSTREAMERPID=`cat $DIR/gstreamer.pid`
-rm $DIR/gstreamer.pid
 
 echo Waiting for GStreamer to starve [ PID = $GSTREAMERPID ]
 while [[ `ps --no-headers $GSTREAMERPID` ]]; do
     sleep 1
 done
+rm $DIR/gstreamer.pid
 
